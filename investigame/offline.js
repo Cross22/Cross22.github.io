5a03f23526460e20024fa38b70acb19e80d5ada1
Marco Grubert <marco@playableworlds.com>
2022-11-08 22:54:19 -0800

    investigame

---
+++ investigame/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1667976285,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tile-sheet0.png",
+		"images/tile-sheet1.png",
+		"images/tile-sheet2.png",
+		"images/mango-sheet0.png",
+		"images/mango-sheet1.png",
+		"images/mango-sheet2.png",
+		"images/playerbox-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
